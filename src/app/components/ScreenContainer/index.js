12b90fa02f550e77c5076464e9fe848ee936e2a2
Siva Guhan S <sivaguhan@Sivas-MacBook-Pro.local>
2024-09-14 11:35:47 +0530

    added screen container

--- src/app/components/ScreenContainer/index.js
+++ src/app/components/ScreenContainer/index.js
@@ -1,3 +1,18 @@
+import { useRouter } from 'next/navigation';
+
 export default function ScreenContainer({ children }) {
-    return <div className="mainContainer">{children}</div>;
+    const router = useRouter();
+    return (
+        <div className="mainContainer">
+            <header className='headerContainer'>
+                <button onClick={() => router.back()} className='backButton'>
+                    &#8592;
+                </button>
+                <span className='headerTitle'>InsureFit</span>
+            </header>
+            <div className="screenContainer">
+                {children}
+            </div>;
+        </div>
+    )
 }
